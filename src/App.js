262408bc7d343e99f1115a73a684b9ca459cdc1c
Nelson Kagimbi <NelsonKagimbi2023@u.northwestern.edu>
2023-04-21 12:57:05 -0500

    add frontend elements

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
+// import Dropdown from 'react-bootstrap/Dropdown'
+// import DropdownButton from 'react-bootstrap/DropdownButton'
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const menuItems = ["Total Population", "Median Household Income", "Per Capita Income", "Under 5yo %"]
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null)
+  const [searchQuery, setSearchQuery] = useState('')
+
+  function handleOptionClick (option) {
+    setSelectedOption(option);
+    setIsOpen(false);
+  }
+  function handleSearchInput (event){
+    setSearchQuery(event.target.value);
+    console.log(searchQuery);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="Header">
+        <h1>Compared-to-What</h1>
+        <div className="Contact-info">
+          <a href="app.js"><button id="Home" onClick>Home</button></a>
+          <button id="Contact">Contact Us</button>
+        </div>
       </header>
+      <div className="searchPart">
+        <input id="searchBox" placeholder="Enter a City Name"
+        autoComplete="off" type="text" value={searchQuery}
+        onChange={handleSearchInput}/>
+       <div className="dropdown-menu">
+        <button onClick={() => setIsOpen(!isOpen)}>
+          {selectedOption || "Filters"}
+        </button>
+        {isOpen && (
+          <ul>
+            {menuItems.map((item) => {
+              return(
+              <li key={item} onClick={()=> handleOptionClick(item)}>
+                {item}
+              </li>
+              
+            );})}
+          </ul>
+        )}
+       </div>
+       <button className='search-button' onClick={() => console.log("clicked search")}>
+        Search</button>
+      </div>
     </div>
   );
 }
